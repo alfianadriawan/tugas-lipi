7a6bbf22281ce7805e55624a2b4032b3b9107739
alfianadriawan <alfian.adriawan@gmail.com>
2021-07-12 22:16:38 +0800

    finishing website

--- resources/views/keltian/profile.blade.php
+++ resources/views/keltian/profile.blade.php
@@ -43,7 +43,7 @@
                                     @else
                                         Perempuan
                                     @endif</span></li>
-                                    <li>Kontak Lainnya <span><a href="https://www.facebook.com" target="_blank" >www.facebook.com</a></span></li>
+                                    <li>ALamat <span>{{$keltian->alamat}}</span></li>
                                 </ul>
                             </div>
                             <div class="text-center"><a href="/keltian/{{$keltian->id}}/edit" class="btn btn-warning">Edit Profile</a></div>
@@ -53,124 +53,23 @@
                     <!-- END LEFT COLUMN -->
                     <!-- RIGHT COLUMN -->
                     <div class="profile-right">
-                        <h4 class="heading">Samuel's Awards</h4>
+                        <h4 class="heading">{{$keltian->nama}} Award's</h4>
                         <!-- END AWARDS -->
                         <!-- TABBED CONTENT -->
-                        <div class="custom-tabs-line tabs-line-bottom left-aligned">
-                            <ul class="nav" role="tablist">
-                                <li class="active"><a href="#tab-bottom-left1" role="tab" data-toggle="tab">Recent Activity</a></li>
-                                <li><a href="#tab-bottom-left2" role="tab" data-toggle="tab">Projects <span class="badge">7</span></a></li>
-                            </ul>
-                        </div>
+                        
                         <div class="tab-content">
                             <div class="tab-pane fade in active" id="tab-bottom-left1">
                                 <ul class="list-unstyled activity-timeline">
                                     <li>
-                                        <i class="fa fa-comment activity-icon"></i>
-                                        <p>Commented on post <a href="#">Prototyping</a> <span class="timestamp">2 minutes ago</span></p>
-                                    </li>
-                                    <li>
-                                        <i class="fa fa-cloud-upload activity-icon"></i>
-                                        <p>Uploaded new file <a href="#">Proposal.docx</a> to project <a href="#">New Year Campaign</a> <span class="timestamp">7 hours ago</span></p>
-                                    </li>
-                                    <li>
-                                        <i class="fa fa-plus activity-icon"></i>
-                                        <p>Added <a href="#">Martin</a> and <a href="#">3 others colleagues</a> to project repository <span class="timestamp">Yesterday</span></p>
+                                        <i class="fa fa-check activity-icon"></i>
+                                        <h4>Prosiding = {{$keltian->prosiding}}</h4>
                                     </li>
                                     <li>
                                         <i class="fa fa-check activity-icon"></i>
-                                        <p>Finished 80% of all <a href="#">assigned tasks</a> <span class="timestamp">1 day ago</span></p>
+                                        <h4>Jurnal = {{$keltian->jurnal}}</h4>
                                     </li>
                                 </ul>
                                 
-                            </div>
-                            <div class="tab-pane fade" id="tab-bottom-left2">
-                                <div class="table-responsive">
-                                    <table class="table project-table">
-                                        <thead>
-                                            <tr>
-                                                <th>Title</th>
-                                                <th>Progress</th>
-                                                <th>Leader</th>
-                                                <th>Status</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            <tr>
-                                                <td><a href="#">Spot Media</a></td>
-                                                <td>
-                                                    <div class="progress">
-                                                        <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%;">
-                                                            <span>60% Complete</span>
-                                                        </div>
-                                                    </div>
-                                                </td>
-                                                <td><img src="assets/img/user2.png" alt="Avatar" class="avatar img-circle"> <a href="#">Michael</a></td>
-                                                <td><span class="label label-success">ACTIVE</span></td>
-                                            </tr>
-                                            <tr>
-                                                <td><a href="#">E-Commerce Site</a></td>
-                                                <td>
-                                                    <div class="progress">
-                                                        <div class="progress-bar" role="progressbar" aria-valuenow="33" aria-valuemin="0" aria-valuemax="100" style="width: 33%;">
-                                                            <span>33% Complete</span>
-                                                        </div>
-                                                    </div>
-                                                </td>
-                                                <td><img src="assets/img/user1.png" alt="Avatar" class="avatar img-circle"> <a href="#">Antonius</a></td>
-                                                <td><span class="label label-warning">PENDING</span></td>
-                                            </tr>
-                                            <tr>
-                                                <td><a href="#">Project 123GO</a></td>
-                                                <td>
-                                                    <div class="progress">
-                                                        <div class="progress-bar" role="progressbar" aria-valuenow="68" aria-valuemin="0" aria-valuemax="100" style="width: 68%;">
-                                                            <span>68% Complete</span>
-                                                        </div>
-                                                    </div>
-                                                </td>
-                                                <td><img src="assets/img/user1.png" alt="Avatar" class="avatar img-circle"> <a href="#">Antonius</a></td>
-                                                <td><span class="label label-success">ACTIVE</span></td>
-                                            </tr>
-                                            <tr>
-                                                <td><a href="#">Wordpress Theme</a></td>
-                                                <td>
-                                                    <div class="progress">
-                                                        <div class="progress-bar" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 75%;">
-                                                            <span>75%</span>
-                                                        </div>
-                                                    </div>
-                                                </td>
-                                                <td><img src="assets/img/user2.png" alt="Avatar" class="avatar img-circle"> <a href="#">Michael</a></td>
-                                                <td><span class="label label-success">ACTIVE</span></td>
-                                            </tr>
-                                            <tr>
-                                                <td><a href="#">Project 123GO</a></td>
-                                                <td>
-                                                    <div class="progress">
-                                                        <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%;">
-                                                            <span>100%</span>
-                                                        </div>
-                                                    </div>
-                                                </td>
-                                                <td><img src="assets/img/user1.png" alt="Avatar" class="avatar img-circle"> <a href="#">Antonius</a></td>
-                                                <td><span class="label label-default">CLOSED</span></td>
-                                            </tr>
-                                            <tr>
-                                                <td><a href="#">Redesign Landing Page</a></td>
-                                                <td>
-                                                    <div class="progress">
-                                                        <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%;">
-                                                            <span>100%</span>
-                                                        </div>
-                                                    </div>
-                                                </td>
-                                                <td><img src="assets/img/user5.png" alt="Avatar" class="avatar img-circle"> <a href="#">Jason</a></td>
-                                                <td><span class="label label-default">CLOSED</span></td>
-                                            </tr>
-                                        </tbody>
-                                    </table>
-                                </div>
                             </div>
                         </div>
                         <!-- END TABBED CONTENT -->
--- resources/views/layouts/includes/_sidebar.blade.php
+++ resources/views/layouts/includes/_sidebar.blade.php
@@ -2,15 +2,15 @@
     <div class="sidebar-scroll">
         <nav>
             <ul class="nav">
-                <li><a href="/dashboard"><i class="lnr lnr-home"></i> <span>Dashboard</span></a></li>
+                <li><a href="/dashboard" class="{{ request()->is('dashboard') ? 'active' : ''}}"><i class="lnr lnr-home"></i> <span>Dashboard</span></a></li>
 
                 @if (auth()->user()->role == 'admin')
-                    <li><a href="/keltian"><i class="lnr lnr-user"></i> <span>Keltian</span></a></li>
+                    <li><a href="/keltian" class="{{ request()->is('keltian') ? 'active' : ''}}"><i class="lnr lnr-user"></i> <span>Keltian</span></a></li>
                 @endif
 
-                <li><a href="/penelitiancrud"><i class="lnr lnr-bookmark"></i> <span>Penelitian</span></a></li>
+                <li><a href="/penelitiancrud" class="{{ request()->is('penelitiancrud') ? 'active' : ''}}"><i class="lnr lnr-bookmark"></i> <span>Penelitian</span></a></li>
 
-                <li><a href="/kunjungancrud"><i class="lnr lnr-book"></i> <span>Kunjungan</span></a></li>
+                <li><a href="/kunjungancrud" class="{{ request()->is('kunjungancrud') ? 'active' : ''}}"><i class="lnr lnr-book"></i> <span>Kunjungan</span></a></li>
             </ul>
         </nav>
     </div>
